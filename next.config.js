c69218e553c47c18dfd8aa5c98c210a8376286a2
baycoretech <creatifycms@gmail.com>
2021-04-20 16:22:50 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = ({
     images: {
-        domains: ['tailwindui.com', 'images.unsplash.com', 'localhost'],
+        domains: ['tailwindui.com', 'images.unsplash.com', 'localhost', 'cockpit.technologiacreativa.com'],
     }
 });
