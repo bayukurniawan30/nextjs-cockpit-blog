dc3927f934244c5f2558701cd4a7052ca3b7e2e6
bayukurniawan30 <baycorephotography@gmail.com>
2021-04-20 11:25:34 +0800

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,19 @@ import Head from "next/head";
 import Layout from "@components/layout";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <title></title>
-        <meta
-          name="Description"
-          content=""
-        />
-      </Head>
+	return (
+		<Layout>
+		<Head>
+			<title></title>
+			<meta
+			name="Description"
+			content=""
+			/>
+		</Head>
 
-      <Component {...pageProps} />
-    </Layout>
-  );
+		<Component {...pageProps} />
+		</Layout>
+	);
 }
 
 export default MyApp;
